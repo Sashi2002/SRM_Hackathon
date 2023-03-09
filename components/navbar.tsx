0b09ca6ebd46b0361d1c8895b45ab0882e3a6847
sashikanta mohanty <sm8168@srmist.edu.in>
2023-03-10 03:59:18 +0530

    :bug: build fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import { AiOutlineCaretDown } from "react-icons/ai";
 import Image from "next/image";
 import srm from "../public/images/srm.png";
-import { MdLanguage } from "react-icons/Md";
+import { MdLanguage } from "react-icons/md";
 import Link from "next/link";
 // import thumbnail from "../public/images/thumbnail.svg";
 const Navbar = () => {
@@ -12,11 +12,11 @@ const Navbar = () => {
           <ul className="mt-4 max-sm:mr-12 max-sm:w-28">
             {/* <Image src={thumbnail} width="120" alt="image" /> */}
           </ul>
-          <span className="max-lg:hidden flex gap-2">
+          <span className="flex gap-2 max-lg:hidden">
             <Image src={srm} alt="image" width="40" />
-            <ul className="pl-1 font-boldn text-3xl">SRM</ul>
+            <ul className="pl-1 text-3xl font-boldn">SRM</ul>
             <ul className="pl-6 mt-2">Events</ul>
-            <ul className="flex mt-1 items-center">
+            <ul className="flex items-center mt-1">
               <AiOutlineCaretDown />
             </ul>
             <ul className="pl-5 mt-2">Facilities</ul>
@@ -27,7 +27,7 @@ const Navbar = () => {
             <ul className="pl-5 mt-2">User Manual</ul>
           </span>
         </div>
-        <div className="flex items-center justify-end w-1/2 gap-6  p-4 md:mx-6">
+        <div className="flex items-center justify-end w-1/2 gap-6 p-4 md:mx-6">
           <span className="flex items-center gap-1 max-sm:hidden">
             <ul>
               <MdLanguage size={25} />
