e31cdedfb46b507d2509ee33eb0dcac4dbac5d54
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-03-09 17:06:09 +0530

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // "./app/**/*.{js,ts,jsx,tsx}",
-    // "./pages/**/*.{js,ts,jsx,tsx}",
-    // "./components/**/*.{js,ts,jsx,tsx}",
- 
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
@@ -12,4 +11,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
