b27495bcf61a07c241d843a3aabe6931acadc48b
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-03-10 10:06:31 +0530

    adminInterface

--- src/pages/hallbooking.tsx
+++ src/pages/hallbooking.tsx
@@ -64,7 +64,7 @@ const Booking = () => {
         <div className="w-64 h-64 flex items-center hover:items-start hover:pt-10 justify-center hover:h-96 hover:w-72 bg-indigo-500">
           <div className="group inline-block relative transition ease-in-out delay-10  hover:-translate-y-3 hover:scale-110 duration-300 ">
             <div className="h-60 w-60">
-              <Image src={tp} className="h-full w-full"></Image>
+              <Image src={tp} alt="image" className="h-full w-full" />
             </div>
 
             <div className="absolute hidden text-white text-m group-hover:block px-14 mt-4 font-semibold">
@@ -90,7 +90,7 @@ const Booking = () => {
         <div className="w-64 h-64 flex items-center hover:items-start hover:pt-10 justify-center hover:h-96 hover:w-72 bg-indigo-500">
           <div className="group inline-block relative transition ease-in-out delay-10  hover:-translate-y-3 hover:scale-110 duration-300 ">
             <div className="h-60 w-60">
-              <Image src={tp} className="h-full w-full"></Image>
+              <Image src={tp} className="h-full w-full" alt="image" />
             </div>
 
             <div className="absolute hidden text-white text-m group-hover:block px-14 mt-4 font-semibold">
@@ -103,7 +103,7 @@ const Booking = () => {
         <div className="w-64 h-64 flex items-center hover:items-start hover:pt-10 justify-center hover:h-96 hover:w-72 bg-indigo-500">
           <div className="group inline-block relative transition ease-in-out delay-10  hover:-translate-y-3 hover:scale-110 duration-300 ">
             <div className="h-60 w-60">
-              <Image src={tp} className="h-full w-full"></Image>
+              <Image src={tp} alt="image" className="h-full w-full" />
             </div>
 
             <div className="absolute hidden text-white text-m group-hover:block px-14 mt-4 font-semibold">
